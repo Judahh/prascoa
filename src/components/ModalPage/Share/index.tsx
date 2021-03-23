5d59077c584490183c68c637a61531ba4198d794
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2021-03-23 08:19:03 -0300

    share done

--- src/components/ModalPage/Share/index.tsx
+++ src/components/ModalPage/Share/index.tsx
@@ -1,13 +1,15 @@
 // file deepcode ignore no-any: any needed
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { default as shareIcon } from '../../../language/share.json';
 import { ModalPageWrapper } from '../styles';
 import LanguageContext from '../../../language/context';
 import { SocialIcon } from 'react-social-icons';
 import { FaRegClone } from 'react-icons/fa';
+import ReactTooltip from 'react-tooltip';
 
 const Share = (props) => {
   const { share } = useContext(LanguageContext);
+  const tooltipRef = useRef<React.RefObject<HTMLElement>>(null);
 
   const navItems: JSX.Element[] = [];
 
@@ -20,6 +22,8 @@ const Share = (props) => {
     temp.select();
     document.execCommand('copy');
     temp.remove();
+    ReactTooltip.show(tooltipRef.current);
+    setTimeout(() => ReactTooltip.hide(tooltipRef.current), 1500);
   };
 
   for (const key in shareIcon) {
@@ -31,26 +35,37 @@ const Share = (props) => {
         navItems.push(
           <SocialIcon
             // prefix={element[0]}
+            key={key}
             network={key}
             bgColor="transparent"
             fgColor="#fff"
             url={share[key]}
+            target="_blank"
           />
         );
       else
         navItems.push(
-          <FaRegClone
+          <div
+            key={key}
             onClick={() => {
-              copy(share[key]);
+              copy(share[key].text);
             }}
             style={{
-              height: '25px',
-              width: '25px',
               display: 'inline-block',
-              verticalAlign: 'middle',
-              margin: '10px',
             }}
-          />
+          >
+            <p ref={tooltipRef} data-tip={share[key].tooltip}></p>
+            <ReactTooltip />
+            <FaRegClone
+              style={{
+                height: '25px',
+                width: '25px',
+                display: 'inline-block',
+                verticalAlign: 'middle',
+                margin: '10px',
+              }}
+            ></FaRegClone>
+          </div>
         );
     }
   }
