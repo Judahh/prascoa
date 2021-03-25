94355f1b8524cc822e72e4fe1ba9424147bf5f9b
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2021-03-25 10:25:39 -0300

    remove unused stuff

--- src/components/Container/index.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-export const Container = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  min-height: 100vh;
-  max-width: 100%;
-  background: ${(props) => props.theme.background};
-`;
